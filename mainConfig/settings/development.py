32698b7ffa536e7b69cfc9de8b9adbc37eb69037
yeazin <naz.yeasin@gmail.com>
2023-02-26 02:07:26 +0600

    Added drf docs and configure postgres

--- mainConfig/settings/development.py
+++ mainConfig/settings/development.py
@@ -13,11 +13,29 @@ DATABASES = {
     }
 }
 
+
+"""
+Postgres setup 
+"""
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
+
 ## APP installed
 INSTALLED_APPS += [
     # third party apps
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
     # installed app
     "accounts.apps.AccountsConfig",
 ]
