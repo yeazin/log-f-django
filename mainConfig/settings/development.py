190cf2ed5540b8075226611a4c020ce6888658ee
yeazin <naz.yeasin@gmail.com>
2023-02-25 15:45:20 +0600

    Project Initialize

---
+++ mainConfig/settings/development.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+ALLOWED_HOSTS = ["*"]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+## APP installed
+INSTALLED_APPS += ["accounts.apps.AccountsConfig"]
+
+## Custom User model Config
+AUTH_USER_MODEL = "accounts.User"
